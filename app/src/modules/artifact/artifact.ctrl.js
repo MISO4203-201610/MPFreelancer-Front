04af1a9e9b2a810b0efbc24fbc25fc137a10da93
alexchacon <alex.chacon@gmail.com>
2016-02-28 22:08:27 -0500

    Artifacts new interfaces
    Artifacts new interfaces

---
+++ app/src/modules/artifact/artifact.ctrl.js
@@ -0,0 +1,44 @@
+(function (ng) {
+    var mod = ng.module('projectModule');
+
+    mod.controller('projectCtrl', ['CrudCreator', '$scope',
+        'projectContext', 'projectModel',
+        function (ngCrud, $scope, url, model) {
+            ngCrud.extendController({
+                name: 'project',
+                displayName: 'Project',
+                ctrl: this,
+                scope: $scope,
+                model: model,
+                url: url
+            });
+            this.loadRefOptions();
+            this.fetchRecords();
+        }]);
+
+    mod.controller('ProjectsexpectedskillsCtrl', ['CrudCreator', '$scope',
+        'skillModel', 'skillContext', 'projectContext',
+        function (ngCrud, $scope, model, url, parentUrl) {
+            ngCrud.extendAggChildCtrl({
+                name: 'expectedskills',
+                displayName: 'Expectedskills',
+                parentUrl: parentUrl,
+                listUrl: url,
+                ctrl: this,
+                scope: $scope,
+                model: model
+            });
+        }]);
+
+    mod.controller('projectListCtrl', ['$scope','Restangular', function ($scope,Restangular) {
+            $scope.getProjectList = function () {
+            Restangular.all("projects/all").getList().then(function (response) {
+                $scope.projects = response;
+            });
+
+        };
+
+            $scope.getProjectList();
+
+        }]);
+})(window.angular);
