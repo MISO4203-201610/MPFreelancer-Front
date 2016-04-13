6cd9ad062b11663e183add56afc5857081f6d12f
Ernesto Nobmann <nobmann88@gmail.com>
2016-04-13 23:28:55 +0000

    Getting all the agreements from a freelancer
    * Now a freelancer can view all his agreements

--- app/src/modules/agreement/agreement.ctrl.js
+++ app/src/modules/agreement/agreement.ctrl.js
@@ -1,4 +1,6 @@
 (function (ng) {
+    //https://github.com/wprl/baucis/blob/master/examples/angular-example-restangular.html
+    //http://stackoverflow.com/questions/18585010/restangular-post-form-data-in-json-format-in-angular-js
     var mod = ng.module('agreementModule');
 
     mod.controller('agreementCtrl', ['CrudCreator', '$scope',
@@ -13,8 +15,31 @@
                 url: url
             });
             this.fetchRecords();
-            Restangular.all("agreements").getList().then(function (response) {
-                $scope.agreements = response;
+            var freelancerResource = Restangular.all("freelancers");
+            freelancerResource.customGET('current').then(function (response) {
+                $scope.freelancer = response;
+                var freelancerId = response.id;
+                console.log('consultando agreements del freelancer' + freelancerId);
+                var resource = Restangular.all("agreements/" + freelancerId + "/agreementsStatus1");
+                resource.getList().then(function (agreementResponse) {
+                    $scope.agreements = agreementResponse;
+                });
             });
+
+            $scope.formData = {};
+            $scope.add = function () {
+                console.log('proyecto aceptado');
+                console.log($scope.formData.proyectoSeleccionado);
+                console.log($scope.formData.monto);
+                console.log($scope.freelancer.id);
+                //var postRequest = Restangular.all('accounts');
+                //resource.post($scope.formData);
+            };
+            $scope.cancel = function () {
+                console.log('cancelar');
+                //                resource.post($scope.formData).then(function (newResource) {
+                //                    $scope.agreements.push(newResource);
+                //                });
+            };
         }]);
 })(window.angular);
