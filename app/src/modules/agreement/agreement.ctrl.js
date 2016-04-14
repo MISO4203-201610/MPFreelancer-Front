ac698ee94e955125d877634a36e3a6d8617153d4
Ernesto Nobmann <nobmann88@gmail.com>
2016-04-14 12:02:57 +0000

    Agreement Interaction with web services
    * Now a freelancer can reply to an invitation
    * Now a freelancer can reject an invitation

--- app/src/modules/agreement/agreement.ctrl.js
+++ app/src/modules/agreement/agreement.ctrl.js
@@ -20,26 +20,45 @@
                 $scope.freelancer = response;
                 var freelancerId = response.id;
                 console.log('consultando agreements del freelancer' + freelancerId);
-                var resource = Restangular.all("agreements/" + freelancerId + "/agreementsStatus1");
+                var resource = Restangular.all("agreements/" + freelancerId + "/agreementsFreelancer");
                 resource.getList().then(function (agreementResponse) {
+                    var arrayLength = agreementResponse.length;
+                    for (var i = 0; i < arrayLength; i++) {
+                        var agreement = agreementResponse[i];
+                        switch (agreement.status) {
+                            case 1:
+                                agreement.status = 'Invited';
+                                break;
+                            case 2:
+                                agreement.status = 'Accepted';
+                                break;
+                            case 3:
+                                agreement.status = 'Rejected';
+                                break;
+                            case 4:
+                                agreement.status = 'Selected';
+                                break;
+                            default:
+                                agreement.status = 'Unknown';
+                                break;
+                        }
+                    }
                     $scope.agreements = agreementResponse;
                 });
             });
-
             $scope.formData = {};
             $scope.add = function () {
+                var agreementId = $scope.formData.proyectoSeleccionado;
+                var monto = $scope.formData.monto;
                 console.log('proyecto aceptado');
-                console.log($scope.formData.proyectoSeleccionado);
-                console.log($scope.formData.monto);
-                console.log($scope.freelancer.id);
-                //var postRequest = Restangular.all('accounts');
-                //resource.post($scope.formData);
+                //Restangular.all("agreements/" + agreementId + "/" + monto + "/agreementsAceptd");
+                Restangular.all("agreements/" + agreementId + "/" + monto + "/agreementsAcept").customPUT();
+
             };
             $scope.cancel = function () {
-                console.log('cancelar');
-                //                resource.post($scope.formData).then(function (newResource) {
-                //                    $scope.agreements.push(newResource);
-                //                });
-            };
+                var agreementId = $scope.formData.proyectoSeleccionado;
+                Restangular.all("agreements/" + agreementId + "/agreementsReject").customPUT();
+            }
+            ;
         }]);
 })(window.angular);
