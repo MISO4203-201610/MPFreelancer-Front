dad4c930affb3a909c79c862a25a45d09643d824
Ernesto Nobmann <nobmann88@gmail.com>
2016-04-17 12:10:24 +0000

    Small fixes to the agreement controller
    * Now the buttons will be referenced by its id

--- app/src/modules/agreement/agreement.ctrl.js
+++ app/src/modules/agreement/agreement.ctrl.js
@@ -1,6 +1,4 @@
 (function (ng) {
-    //https://github.com/wprl/baucis/blob/master/examples/angular-example-restangular.html
-    //http://stackoverflow.com/questions/18585010/restangular-post-form-data-in-json-format-in-angular-js
     var mod = ng.module('agreementModule');
 
     mod.controller('agreementCtrl', ['CrudCreator', '$scope',
@@ -53,12 +51,44 @@
                 console.log('proyecto aceptado');
                 //Restangular.all("agreements/" + agreementId + "/" + monto + "/agreementsAceptd");
                 Restangular.all("agreements/" + agreementId + "/" + monto + "/agreementsAcept").customPUT();
-
+                $scope.refresh();
             };
             $scope.cancel = function () {
                 var agreementId = $scope.formData.proyectoSeleccionado;
                 Restangular.all("agreements/" + agreementId + "/agreementsReject").customPUT();
-            }
-            ;
+                $scope.refresh();
+            };
+            $scope.refresh = function () {
+                var refreshResource = Restangular.all("freelancers");
+                refreshResource.customGET('current').then(function (response) {
+                    $scope.freelancer = response;
+                    var freelancerId = response.id;
+                    var resource = Restangular.all("agreements/" + freelancerId + "/agreementsFreelancer");
+                    resource.getList().then(function (agreementResponse) {
+                        var arrayLength = agreementResponse.length;
+                        for (var i = 0; i < arrayLength; i++) {
+                            var agreement = agreementResponse[i];
+                            switch (agreement.status) {
+                                case 1:
+                                    agreement.status = 'Invited';
+                                    break;
+                                case 2:
+                                    agreement.status = 'Accepted';
+                                    break;
+                                case 3:
+                                    agreement.status = 'Rejected';
+                                    break;
+                                case 4:
+                                    agreement.status = 'Selected';
+                                    break;
+                                default:
+                                    agreement.status = 'Unknown';
+                                    break;
+                            }
+                        }
+                        $scope.agreements = agreementResponse;
+                    });
+                });
+            };
         }]);
 })(window.angular);
